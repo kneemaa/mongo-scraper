0d29790e6f1d17e988e4ee63a2089baf8890f194
Nema Darban <nema.darban@gmail.com>
2018-04-04 22:04:44 -0700

    mongo db connection added

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const bodyParser = require("body-parser")
 const exphbs = require("express-handlebars")
 const routes = require("./routes/routes.js")
 const requestPromise = require('request-promise')
-
-
+const mongoose = require("mongoose")
+const Notes = require("./models/notes")
+const Articles = require("./models/article")
+const db = {
+	Notes: Notes,
+	Articles: Articles
+}
 
 const PORT = process.env.PORT || 3000
 app.use(express.static("public"))
@@ -14,6 +19,7 @@ app.use(bodyParser.json())
 app.engine("handlebars", exphbs({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
 
+mongoose.connect("mongodb://localhost/scraperdb")
 
 const request = require('request')
 const cheerio = require('cheerio')
